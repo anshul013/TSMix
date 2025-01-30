cb817cb4a42096b50aefb26ce8ba7bce4308be1b
anshul013 <ultimatenanshul@gmail.com>
2025-01-30 17:30:06 +0100

    Replaced tf.transpose() being directly applied to a KErasTensor with a Lambda fn in tsmixer_rev_in

--- models/tsmixer_rev_in.py
+++ models/tsmixer_rev_in.py
@@ -42,9 +42,8 @@ def build_model(
 
   if target_slice:
     x = x[:, :, target_slice]
-
-  x = tf.transpose(x, perm=[0, 2, 1])  # [Batch, Channel, Input Length]
+  x = layers.Lambda(lambda t: tf.transpose(t, perm=[0, 2, 1]))(x) # [Batch, Channel, Input Length]
   x = layers.Dense(pred_len)(x)  # [Batch, Channel, Output Length]
-  outputs = tf.transpose(x, perm=[0, 2, 1])  # [Batch, Output Length, Channel])
+  outputs = layers.Lambda(lambda t: tf.transpose(t, perm=[0, 2, 1]))(x) # [Batch, Output Length, Channel])
   outputs = rev_norm(outputs, mode='denorm', target_slice=target_slice)
   return tf.keras.Model(inputs, outputs)
